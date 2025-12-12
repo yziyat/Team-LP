795482e0a06e13851b4e77d9bd0718af3ea79f4d
yziyat <yziyat@outlook.com>
2025-12-12 16:12:21 +0100

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: '/Team-LP/', // Nom exact de votre dépôt GitHub
-=======
-  base: '/Team-LP/', // Repository name for GitHub Pages
->>>>>>> 2018618667891a8668c5577f38d0eee3da78abfa
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
